f059cb792e09726da3bb080c73dbd6a7f0f4ad89
fredeos <fobando@estudiantec.cr>
2024-09-16 23:12:27 -0600

    Gramatica #2

--- src/dictionary.pl
+++ src/dictionary.pl
@@ -1,28 +1,33 @@
-% Hechos: En este es el vocabulario que conoce el programa
-/*
-Los 'determinantes' estan definidos como (palabra,genero) esto permite que los valores de determinante y
-un nombre tengan el mismo genero para que tengan sentido
-*/
-determinante(el, m).
+% -------------------------------[ Determinates ]-------------------------------
+determinante(el, m). % HECHO: determinante(palabra, genero) = es un determinante conocido clasificado por genero
 determinante(la, f).
 
-/*
-Los 'nombres' estan definindos como (nombre,genero) para poder conectarla aun determinante del
-mismo genero
-*/
-nombre(hombre, m).
+% -------------------------------[ Nombres o sujetos ]-------------------------------
+nombre(hombre, m). % HECHO: nombre(palabra, genero) = es un nombre conocido clasificado por genero
 nombre(manzana, f).
 nombre(casa, f).
 
-verbo(comer).
+% -------------------------------[ Verbos y derivados]-------------------------------
+% >> Verbos
+verbo(V):- conjugado(_,V). % REGLA: verbo(conjugado) = analiza si un verbo es un conjugado de otro
+verbo(comer).   % HECHO: verbo(palabra) = es una palabra conocida
 verbo(estar).
-verbo(V):- conjugado(_,V).
+verbo(tener).
 
-conjugado(comer, come).
+% >> Conjugaciones de verbos
+conjugado(comer, come). % HECHO: conjugado(verbo, palabra) = es una conjugacion conocida de un verbo
+conjugado(estar, esta).
+conjugado(tener, tiene).
 
-auxiliar(en).
+% >> Auxiliares de verbos
+auxiliar(en). %HECHO: auxiliar(palabra) = es un auxiliar conocido
 
-% Reglas: Construccion de sintagmas y oraciones para el programa
-sintagma_nominal(D,N):- determinante(D,G), nombre(N,G).
-sintagma_verbal(V,D,N):- verbo(V), sintagma_nominal(D,N).
-oracion(A,B,C,D,E):- sintagma_nominal(A,B), sintagma_verbal(C,D,E), B\==E.
+% -------------------------------[ Composicion de oraciones ]-------------------------------
+% Sintagmas
+sintagma_nominal(D,N):- determinante(D,G), nombre(N,G).   %REGLA:
+sintagma_nominal([yo]). %HECHO: sintagma_nominal(palabra) = indica una palabra unica que funciona como sintagma nominal
+
+sintagma_verbal(V,D,N):- verbo(V), sintagma_nominal(D,N). %REGLA:
+
+% Oraciones
+oracion(A,B,C,D,E):- sintagma_nominal(A,B), sintagma_verbal(C,D,E), B\==E, write([A,B,C,D,E]). %REGLA:
