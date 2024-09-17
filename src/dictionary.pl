6f4434d58668c80d21ec0cf96e590d76a10ec598
fredeos <fobando@estudiantec.cr>
2024-09-16 22:53:59 -0600

    Gramatica #1

--- src/dictionary.pl
+++ src/dictionary.pl
@@ -1,22 +1,28 @@
 % Hechos: En este es el vocabulario que conoce el programa
+/*
+Los 'determinantes' estan definidos como (palabra,genero) esto permite que los valores de determinante y
+un nombre tengan el mismo genero para que tengan sentido
+*/
+determinante(el, m).
+determinante(la, f).
 
-% /////////// Palabras unicas
-determinante(el).
-determinante(la).
+/*
+Los 'nombres' estan definindos como (nombre,genero) para poder conectarla aun determinante del
+mismo genero
+*/
+nombre(hombre, m).
+nombre(manzana, f).
+nombre(casa, f).
 
-nombre(hombre).
-nombre(mujer).
-nombre(persona).
+verbo(comer).
+verbo(estar).
+verbo(V):- conjugado(_,V).
 
-verbo(come).
-verbo(necesita).
-verbo(huye).
-verbo(tiene).
+conjugado(comer, come).
 
+auxiliar(en).
 
-% ////////// Casos particulares
-sintagma_nominal(yo).
-
-
-% Reglas: Construccion de sintagmas y oraciones
-sintagma_nominal(D,N):- determinante(D), nombre(N).
+% Reglas: Construccion de sintagmas y oraciones para el programa
+sintagma_nominal(D,N):- determinante(D,G), nombre(N,G).
+sintagma_verbal(V,D,N):- verbo(V), sintagma_nominal(D,N).
+oracion(A,B,C,D,E):- sintagma_nominal(A,B), sintagma_verbal(C,D,E), B\==E.
